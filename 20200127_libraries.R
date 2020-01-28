66e04db1139c3a787c2667d62d74c47d2eba1368
AETurnbull <alisonturnbull@gmail.com>
2020-01-27 19:39:11 -0500

    Added folders for data and figures and completed code.

--- 20200127_libraries.R
+++ 20200127_libraries.R
@@ -19,5 +19,21 @@ dta<-dta %>% select(Package, LibPath, Version, Priority, Built)
 ## hint: readr::write_csv() or write.table()
 ## idea: try using here::here() to create the file path
 
-readr::write_csv(dta, here::here("installed-packages.csv"))
+readr::write_csv(dta, here::here("data", "installed-packages.csv"))
 
+apt <- dta %>%
+  filter(is.na(Priority)) %>%
+  select(Package, Built)
+
+apt_freqtable <- apt %>%
+  count(Built) %>%
+  mutate(prop = n / sum(n))
+
+readr::write_csv(apt_freqtable, here::here("data", "add-on-packages-freqtable.csv"))
+
+
+## if you use ggplot2, code like this will work:
+ggplot(apt_freqtable, aes(x = Built, y = n)) +
+  geom_col()
+
+ggsave(here::here("figs", "built-barchart.png"))
